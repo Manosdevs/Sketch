d71c72307d3ffbe29504495aacb7d75b69c2377c
Manosdevs <manosdevs@gmail.com>
2022-04-17 23:27:20 +0300

    colors!

--- app.js
+++ app.js
@@ -2,9 +2,25 @@ const container = document.getElementById('container');
 for (let i = 0; i < (16 * 16); i++) {
     
     let content = document.createElement('div');
-    content.innerText = (i + 1);
+    content.innerText = " ";
     container.appendChild(content).className = "grid-item";
     
     
 };
+let brush = 'red';
 
+const btn = document.querySelectorAll('button');
+btn.forEach(elem => elem.addEventListener('click', function(e){
+ brush = e.target.innerText;
+}));
+
+
+const chng = document.querySelectorAll('.grid-item');
+console.log(chng);
+chng.forEach(elem => elem.addEventListener('mouseover', function (e)  {
+    e.target.style.backgroundColor = brush;
+
+}));
+chng.forEach(elem => elem.addEventListener('click', function (e)  {
+    e.target.style.backgroundColor = 'white';
+}));   
