776478c7ddb94f99daafa0d47955b0fd47e4d509
Manosdevs <manosdevs@gmail.com>
2022-04-18 03:43:26 +0300

    styles and more!

--- app.js
+++ app.js
@@ -1,26 +1,43 @@
-const container = document.getElementById('container');
+const cont = document.getElementById('cont');
 for (let i = 0; i < (16 * 16); i++) {
     
     let content = document.createElement('div');
     content.innerText = " ";
-    container.appendChild(content).className = "grid-item";
+    content.classList.add('grid-item')
+    cont.appendChild(content);
     
     
 };
 let brush = 'red';
+let paint = false;
 
-const btn = document.querySelectorAll('button');
+const btn = document.querySelectorAll('.btn');
 btn.forEach(elem => elem.addEventListener('click', function(e){
  brush = e.target.innerText;
 }));
 
 
+
 const chng = document.querySelectorAll('.grid-item');
 console.log(chng);
 chng.forEach(elem => elem.addEventListener('mouseover', function (e)  {
-    e.target.style.backgroundColor = brush;
+   if (paint === true) {
+   e.target.style.backgroundColor = brush;
+   document.getElementById('cont').style.cursor = 'crosshair';
+   }else {
+    document.getElementById('cont').style.cursor = 'default';
+   }
+})); 
 
-}));
+const stc = document.body;
+stc.addEventListener('keypress', () => {
+    paint = !paint;
+    console.log(paint);
+});
+
+
+
+
 chng.forEach(elem => elem.addEventListener('click', function (e)  {
     e.target.style.backgroundColor = 'white';
 }));   
