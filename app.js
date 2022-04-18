4259dbe506366c68dd29bfacdf9c634a0743dd51
Manosdevs <manosdevs@gmail.com>
2022-04-18 16:45:51 +0300

    dynamic

--- app.js
+++ app.js
@@ -1,20 +1,48 @@
+
+
 const cont = document.getElementById('cont');
-for (let i = 0; i < (16 * 16); i++) {
-    
-    let content = document.createElement('div');
-    content.innerText = " ";
-    content.classList.add('grid-item')
-    cont.appendChild(content);
-    
-    
+
+let prevrows = 16;
+let prevcols = 16;
+
+function sketch(rows,columns) {
+    cont.style.setProperty('--grid-row', rows);
+    cont.style.setProperty('--grid-column', columns);
+
+    for (let i = 0; i < (columns * rows); i++) {
+        
+        let content = document.createElement('div');
+        content.innerText = " ";
+        content.classList.add('grid-item')
+        cont.appendChild(content);
+        
+        
+    };
 };
-let brush = 'red';
+
+sketch(16,16);
+
+const inp1 = document.getElementById('forminput');
+const inp2 = document.getElementById('forminput2');
+const go = document.getElementById('go');
+
+go.onclick = function () {
+    sketch(inp1.value, inp2.value);
+
+}
+
+
+
+
+let brush = 'white';
 let paint = false;
 
-const btn = document.querySelectorAll('.btn');
+const btn = document.querySelectorAll('.clr');
 btn.forEach(elem => elem.addEventListener('click', function(e){
  brush = e.target.innerText;
 }));
+
+
 
 
 
@@ -35,7 +63,8 @@ stc.addEventListener('keypress', () => {
     console.log(paint);
 });
 
-
+const clear = document.querySelector('.btn-dark');
+clear.onclick = () => chng.forEach(elem => elem.style.backgroundColor = 'white');
 
 
 chng.forEach(elem => elem.addEventListener('click', function (e)  {
