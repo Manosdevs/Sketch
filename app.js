915d78edf4b9f96f91e855710b6e5b7aa711b933
Manosdevs <manosdevs@gmail.com>
2022-04-18 17:08:36 +0300

    pretty much done!

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ let prevcols = 16;
 function sketch(rows,columns) {
     cont.style.setProperty('--grid-row', rows);
     cont.style.setProperty('--grid-column', columns);
+    prevrows = rows;
+    prevcols = columns;
 
     for (let i = 0; i < (columns * rows); i++) {
         
@@ -21,21 +23,35 @@ function sketch(rows,columns) {
 };
 
 sketch(16,16);
+turnon();
 
 const inp1 = document.getElementById('forminput');
 const inp2 = document.getElementById('forminput2');
+
+
 const go = document.getElementById('go');
 
+function rmv() {
+    for (let i = 0; i < (prevcols * prevrows); i++) {
+        const content = document.querySelectorAll('.grid-item');
+        content.forEach(elem => elem.remove());
+    };
+};
+
 go.onclick = function () {
+    
+
+    rmv();
     sketch(inp1.value, inp2.value);
+    turnon();
 
 }
 
 
 
 
-let brush = 'white';
-let paint = false;
+let brush = 'red';
+let paint = true;
 
 const btn = document.querySelectorAll('.clr');
 btn.forEach(elem => elem.addEventListener('click', function(e){
@@ -45,17 +61,25 @@ btn.forEach(elem => elem.addEventListener('click', function(e){
 
 
 
+function turnon(){
+    const chng = document.querySelectorAll('.grid-item');
+    console.log(chng);
+    chng.forEach(elem => elem.addEventListener('mouseover', function (e)  {
+    if (paint === true) {
+    e.target.style.backgroundColor = brush;
+    document.getElementById('cont').style.cursor = 'crosshair';
+    }else {
+        document.getElementById('cont').style.cursor = 'default';
+    }
+}))
+    const clear = document.querySelector('.btn-dark');
+    clear.onclick = () => chng.forEach(elem => elem.style.backgroundColor = 'white');
 
-const chng = document.querySelectorAll('.grid-item');
-console.log(chng);
-chng.forEach(elem => elem.addEventListener('mouseover', function (e)  {
-   if (paint === true) {
-   e.target.style.backgroundColor = brush;
-   document.getElementById('cont').style.cursor = 'crosshair';
-   }else {
-    document.getElementById('cont').style.cursor = 'default';
-   }
-})); 
+
+    chng.forEach(elem => elem.addEventListener('click', function (e)  {
+    e.target.style.backgroundColor = 'white';
+}));  
+}; 
 
 const stc = document.body;
 stc.addEventListener('keypress', () => {
@@ -63,10 +87,4 @@ stc.addEventListener('keypress', () => {
     console.log(paint);
 });
 
-const clear = document.querySelector('.btn-dark');
-clear.onclick = () => chng.forEach(elem => elem.style.backgroundColor = 'white');
-
-
-chng.forEach(elem => elem.addEventListener('click', function (e)  {
-    e.target.style.backgroundColor = 'white';
-}));   
+ 
